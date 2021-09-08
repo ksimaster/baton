652e78d9249f035920f14278e9b35c8745366f57
ksimaster <ksimasterlord@gmail.com>
2021-09-08 21:23:33 +0300

    Тест таймера на корутине
    Сделан тестовый таймер на корутине. вкл. и отключение паузы проверено. Подключение выбранного времени не настроено

--- TimeButton/Assets/Scripts/SetTimerScript.cs
+++ TimeButton/Assets/Scripts/SetTimerScript.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class SetTimerScript : MonoBehaviour
 {
     public Text second;
-    public int setSecond = 0;
+    public float setSecond = 10;
     public Text playerSetSecond;
 
 
@@ -20,6 +20,38 @@ public class SetTimerScript : MonoBehaviour
     {
 
         second.text = playerSetSecond.text;
+    }
+
+    public void StartTimer()
+    {
+        StartCoroutine(Timer(setSecond));
+    }
+
+    public void StartPause()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void StopPause()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void StopTimer()
+    {
+        StopAllCoroutines();
+        setSecond = 0f;
+    }
+
+    IEnumerator Timer(float timerTime)
+    {
+        while (timerTime > 0)
+        {
+            yield return new WaitForSeconds(1f);
+            timerTime--;
+            second.text = timerTime.ToString();
+        }
+
     }
 
 }
