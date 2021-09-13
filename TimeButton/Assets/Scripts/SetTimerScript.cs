abc9a276b0931e500b249377da7bfe18cef23089
ksimaster <ksimasterlord@gmail.com>
2021-09-13 21:57:05 +0300

    Доделан прототип таймера
    Доделан прототип таймера пользовательского значения, реализовано 2 версии таймера по корутине и без неё. Код избыточен, требует чистки

--- TimeButton/Assets/Scripts/SetTimerScript.cs
+++ TimeButton/Assets/Scripts/SetTimerScript.cs
@@ -8,8 +8,18 @@ public class SetTimerScript : MonoBehaviour
     public Text second;
     public float setSecond = 10;
     public Text playerSetSecond;
+    private bool runTimer = false;
+    public Button setTimerButton;
 
+    private void FixedUpdate()
+    {
+        if (runTimer) Timer2(setSecond);
+    }
 
+    public void OffSetTimerButton()
+    {
+        setTimerButton.transform.position = new Vector3(2000, 3000, 0);
+    }
     public void SetTime()
     {
 
@@ -24,6 +34,30 @@ public class SetTimerScript : MonoBehaviour
 
     public void StartTimer()
     {
+        second.text = setSecond.ToString();
+        StartCoroutine(Timer(setSecond));
+    }
+
+    public void StartPlayerTimer()
+    {
+     /*   try
+        { */
+            setSecond = float.Parse(playerSetSecond.text);
+     
+            Debug.Log(setSecond);
+        /*}
+        catch (ArgumentNullException e)
+        {
+            Debug.Log(e);
+            playerSetSecond.text = "Wrong Value";
+        } */
+       
+        second.text = setSecond.ToString();
+        //решение без корутины
+
+
+
+        //решение с корутиной
         StartCoroutine(Timer(setSecond));
     }
 
@@ -41,6 +75,7 @@ public class SetTimerScript : MonoBehaviour
     {
         StopAllCoroutines();
         setSecond = 0f;
+        second.text = setSecond.ToString();
     }
 
     IEnumerator Timer(float timerTime)
@@ -52,6 +87,22 @@ public class SetTimerScript : MonoBehaviour
             second.text = timerTime.ToString();
         }
 
+    }
+
+    public void Timer2(float timerTime)
+    {
+        while (timerTime > 0)
+        {
+            
+            timerTime-=Time.deltaTime;
+            second.text = timerTime.ToString();
+        }
+    }
+
+    public void TrowTimer()
+    {
+        setSecond = 0;
+        Timer2(setSecond);
     }
 
 }
---
+++ TimeButton/Assets/Scripts/TestScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+  
+    void Update()
+    {
+        Debug.Log(Time.timeScale);
+    }
+}
